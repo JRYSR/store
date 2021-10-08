a5b485fb25b11d7ffe5485044a5197ce48affc76
JRYSR <hbx9610232021@163.com>
2021-10-08 16:39:45 +0800

    1

--- python自动化/day01/京东.py
+++ python自动化/day01/京东.py
@@ -8,4 +8,8 @@ driver.find_element_by_id("key").send_keys("歼20")
 driver.find_element_by_xpath('//*[@id="search"]/div/div[2]/button').click()
 
 time.sleep(10)
+
+driver.find_element_by_xpath('//*[@id="J_goodsList"]/ul/li[1]/div/div[1]/a').click()
+time.sleep(10)
+
 driver.quit()
